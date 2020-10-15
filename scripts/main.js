b63ea81be370c3a64ac1bac4651ad31f84c16cf5
MarilenaRoque <marilenas@id.uff.br>
2020-10-15 14:19:29 -0300

    Build function to display Books
    Built showBooks, displayBook and createTd
    The functions create a table to display the books on the page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 
+// Book Constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,11 +8,44 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+// Function to get Book info from the form and create a new Book instance
 function addBookToLibrary() {
-  title = document.getElementById('title').value;
-  author = document.getElementById('author').value;
-  pages =document.getElementById('pages').value;
-  read = document.getElementById('read').checked;
-  newBook = new Book(title, author, pages, read);
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages =document.getElementById('pages').value;
+  let read = document.getElementById('read').checked;
+  let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  
+  showBooks();
 }
+
+// Function to Display the Books
+function showBooks() {
+    console.log(myLibrary);
+    myLibrary.forEach(displayBook);
+}
+
+function displayBook(book) {
+    const table = document.getElementById('display-books') 
+    let newTr = document.createElement('tr');
+    table.appendChild(newTr);
+    createTd(book.title, newTr)
+    createTd(book.author, newTr)
+    createTd(book.pages, newTr)
+    let status = "";
+    if (book.read === false) {
+        status = "Not Read";
+    } else {
+        status = "Read";
+    }
+    createTd(status, newTr);
+}
+function createTd(text, tr) {
+     let newTd = document.createElement('td');
+     newTd.textContent = text;
+     tr.appendChild(newTd);
+}
+
+
+
