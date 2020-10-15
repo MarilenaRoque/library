9de7143422a9195ac91762f37f7c8ae033db705e
MarilenaRoque <marilenas@id.uff.br>
2020-10-15 12:43:38 -0300

    AddBookToLibrary function built
    Build function to create a new Book Object
    Set an array to store it

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
+let myLibrary = [];
 
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+  title = document.getElementById('title').value;
+  author = document.getElementById('author').value;
+  pages =document.getElementById('pages').value;
+  read = document.getElementById('read').checked;
+  newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
